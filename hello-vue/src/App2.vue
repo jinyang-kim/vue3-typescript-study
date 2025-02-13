2be3148a3c8359e7d34ddb9512258d625cf94d00
jinyang_kim <sunlover011@gmail.com>
2025-02-14 00:50:31 +0900

    first commit

---
+++ hello-vue/src/App2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>안녕하세요~ {{name}} 님!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const name = ref("홍길동");
+</script>
+
+<style scoped>
+
+</style>
