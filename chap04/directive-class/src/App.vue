58e4fa4e2d2917b172def0a49b2b6c08f0150c33
jinyang_kim <sunlover011@gmail.com>
2025-03-31 01:43:44 +0900

    feat: 4.1.7 class 속성 바인딩

---
+++ chap04/directive-class/src/App.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import {computed, ref} from 'vue';
+
+const msg = ref("안녕하세요");
+const isTextColorRed = ref(true);
+const isBgColorBlue = ref(false);
+const styles = ref({
+  textColorRed: false,
+  bgColorBlue: true
+});
+const computedStyles = computed((): {textColorRed: boolean; bgColorBlue: boolean;} => {
+  // 무작위 값을 이용하여 0또는 1을 생성 (textColorRed용)
+  const randText = Math.round(Math.random());
+  // textColorRed 속성의 값을 나타내는 변수를 true로 준비
+  let textColorFlg = true;
+  // 발생한 무작위값이 0이면 false로 변경
+  if (randText == 0) {
+    textColorFlg = false;
+  }
+  // 무작위 값을 이용하여 0 또는 1을 생성 (bgColorBlue용)
+  const randBg = Math.round(Math.random());
+  // bgColorBlue 속성의 값을 나타내는 변수를 true로 준비
+  let bgColorFlg = true;
+  // 발생한 무작위 값이 0이면 false로 변경
+  if (randBg == 0) {
+    bgColorFlg = false;
+  }
+  // 각 속성의 값을 객체로 변환
+  return {
+    textColorRed: textColorFlg,
+    bgColorBlue: bgColorFlg
+  }
+})
+</script>
+
+<template>
+  <div>
+    <p v-bind:class="{textColorRed: true, bgColorBlue: true}">{{msg}}</p>
+    <p v-bind:class="{textColorRed: isTextColorRed, bgColorBlue: isBgColorBlue}">{{msg}}</p>
+    <p v-bind:class="{textColorPink: true}">{{msg}}</p>
+    <p v-bind:class="{'text-color-pink': true}">{{msg}}</p>
+    <p class="textSize24" v-bind:class="{textColorRed: isTextColorRed, bgColorBlue: isBgColorBlue}">{{msg}}</p>
+    <p class="textSize24" v-bind:class="styles">{{msg}}</p>
+    <p v-bind:class="computedStyles">{{msg}}</p>
+  </div>
+</template>
+
+<style scoped>
+.textColorRed {
+  color: red;
+}
+.text-color-pink {
+  color: pink;
+}
+.bgColorBlue {
+  background-color: blue;
+}
+.textSize24 {
+  font-size: 24px;
+}
+</style>
---
+++ chap04/directive-style/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {computed, ref} from 'vue';
+
+const msg = ref("안녕하세요");
+const msgTextRed = ref("red");
+const msgTextColor = ref("white");
+const msgBgColor = ref("black");
+const msgStyles = ref({
+  color: "white",
+  backgroundColor: "black"
+});
+const msgStyles2 = ref({
+  fontSize: "24pt"
+});
+const msgStyles3 = ref({
+  color: "pink",
+  fontSize: "24pt"
+});
+const textSize = computed((): string => {
+  const size = Math.round(Math.random() * 25) + 10;
+  return `${size}pt`;
+});
+</script>
+
+<template>
+  <div>
+    <p v-bind:style="{color: msgTextRed}">{{msg}}</p>
+    <p v-bind:style="{color: 'pink'}">{{msg}}</p>
+    <p v-bind:style="{fontSize: textSize}">{{msg}}</p>
+    <p v-bind:style="{color: msgTextColor, backgroundColor: msgBgColor}">{{msg}}</p>
+    <p v-bind:style="{color: msgTextColor, 'background-color': msgBgColor}">{{msg}}</p>
+    <p v-bind:style="msgStyles">{{msg}}</p>
+    <p v-bind:style="[msgStyles, msgStyles2]">{{msg}}</p>
+    <p v-bind:style="[msgStyles, msgStyles3]">{{msg}}</p>
+    <p v-bind:style="[msgStyles3, msgStyles]">{{msg}}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
