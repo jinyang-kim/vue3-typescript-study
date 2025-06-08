a6adc68cb2b65759ef1b1f4f7ee2d66693fe6cee
jinyang_kim <sunlover011@gmail.com>
2025-06-09 02:12:14 +0900

    feat: chap10 10.3.3 경로 파라미터를 Props로 가져오기

---
+++ chap10/router-basic/src/views/member/MemberDetail.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import {computed, inject} from 'vue';
+import { RouterLink } from 'vue-router';
+import type { Member } from '@/interfaces';
+
+interface Props {
+  id: number;
+}
+
+const props = defineProps<Props>();
+const memberList = inject("memberList") as Map<number, Member>;
+const member = computed(
+  (): Member => {
+    return memberList.get(props.id) as Member;
+  }
+)
+const localNote = computed(
+  (): string => {
+    let localNote = "--";
+    if(member.value.note != undefined) {
+      localNote = member.value.note;
+    }
+
+    return localNote;
+  }
+)
+</script>
+
+<template>
+  <h1>회원 관리</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>
+        <RouterLink v-bind:to="{name: 'AppTop'}">Top</RouterLink>
+      </li>
+      <li>
+        <RouterLink v-bind:to="{name: 'MemberList'}">회원 리스트</RouterLink>
+      </li>
+      <li>회원 상세 정보</li>
+    </ul>
+  </nav>
+  <section>
+    <h2>회원 상세 정보</h2>
+    <dl>
+      <dt>ID</dt>
+      <dd>{{member.id}}</dd>
+      <dt>이름</dt>
+      <dd>{{member.name}}</dd>
+      <dt>메일주소</dt>
+      <dd>{{member.email}}</dd>
+      <dt>보유 포인트</dt>
+      <dd>{{member.points}}</dd>
+      <dt>비고</dt>
+      <dd>{{localNote}}</dd>
+    </dl>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
--- chap10/router-basic/src/views/member/MemberList.vue
+++ chap10/router-basic/src/views/member/MemberList.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
+import { inject } from 'vue';
+import { RouterLink } from 'vue-router';
+import type { Member } from '@/interfaces';
 
+const memberList = inject("memberList") as Map<number, Member>;
 </script>
 
 <template>
   <h1>회원 관리</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>
+        <RouterLink v-bind:to="{name: 'AppTop'}">Top</RouterLink>
+      </li>
+      <li>회원 리스트</li>
+    </ul>
+  </nav>
   <section>
     <h2>회원 리스트</h2>
+    <section>
+      <ul>
+        <li v-for="[id, member] in memberList" v-bind:key="id">
+          <RouterLink v-bind:to="{name: 'MemberDetail', params: {id: id}}">
+            ID가 {{id}}인 {{member.name}} 님
+          </RouterLink>
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
 
